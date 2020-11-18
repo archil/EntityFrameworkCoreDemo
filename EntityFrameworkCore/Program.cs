b690068003c031fb274a60b2753e719d0c9f5d8a
Archil Varshanidze <archil.varshanidze@wandiocom>
2020-11-18 20:03:57 +0400

    added draft version

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -9,89 +9,16 @@ namespace EntityFrameworkCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Entity Framework!");
-
-
-            Subject math = new Subject()
-            {
-                Name = "Math"
-            };
-
-            Subject programming = new Subject()
-            {
-                Name = "Programming"
-            };
-
-            Student archil = new Student
-            {
-                Name = "Archil"
-            };
-
-            Student giorgi = new Student
-            {
-                Name = "George"
-            };
-
-            List<StudentSubject> studentSubjects = new List<StudentSubject>()
-            {
-                new StudentSubject() { Student = archil, Subject = math },
-                new StudentSubject() { Student = archil, Subject = programming },
-            };
-
-            archil.Subjects = studentSubjects;
-            giorgi.Subjects = new List<StudentSubject>()
-            {
-                new StudentSubject() { Student = giorgi, Subject = math },
-            };
-
-            using (BloggingDbContext db = new BloggingDbContext())
-            {
-                db.Students.Add(archil);
-                db.Students.Add(giorgi);
-
-                db.SaveChanges();
-            }
-
-            using (var db = new BloggingDbContext())
-            {
-
-                for (int i = 0; i < 50; i++)
-                {
-                    db.Students.Add(new Student()
-                    {
-                        Name = "Student 1",
-                        Subjects = new List<StudentSubject>
-                        {
-                            new StudentSubject{ Subject = new Subject{  Name = "c#" } }
-                        }
-                    });
-                }
-
-                for (int i = 0; i < 5; i++)
-                {
-                    db.Subjects.Add(new Subject()
-                    {
-                        Name = "Subject 1"
-                    });
-                }
-
-                db.SaveChanges();
-
-                var studentCount = db.Students.Count();
-
-
-                Console.WriteLine($"There are {studentCount} students in database");
-            }
-
-            Console.ReadLine();
         }
     }
 
     public class BloggingDbContext : DbContext
     {
-        public DbSet<Student> Students { get; set; }
-        public DbSet<StudentSubject> StudentSubjects { get; set; }
-        public DbSet<Subject> Subjects { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -99,28 +26,33 @@ namespace EntityFrameworkCore
         }
     }
 
-    public class Student
+    public class Person
     {
-        public int StudentId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public List<StudentSubject> Subjects { get; set; }
     }
 
-    public class StudentSubject
+    public class Car
     {
-        public int StudentSubjectId { get; set; }
-
-        public int StudentId { get; set; }
-        public virtual Student Student { get; set; }
-
-        public int SubjectId { get; set; }
-        public virtual Subject Subject { get; set; }
+        public string LicencePlateNumber { get; set; }
     }
 
-    public class Subject
+    public class Blog
+    { 
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+        public decimal Rating { get; set; }
+        public string Author { get; set; }
+        public List<Post> Posts { get; set; }
+    }
+
+    public class Post
     {
-        public int SubjectId { get; set; }
-        public string Name { get; set; }
-        public List<StudentSubject> Students { get; set; }
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int MyBlogForeignKeyValue { get; set; }
+        public Blog Blog { get; set; }
     }
 }
