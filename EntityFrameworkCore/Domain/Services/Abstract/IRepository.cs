251ef4bf3953274725198b5a2d2502cd93bf9108
Archil Varshanidze <archil.varshanidze@wandiocom>
2020-12-04 20:56:54 +0400

    added interfaces

---
+++ EntityFrameworkCore/Domain/Services/Abstract/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using LearningManagementSystem.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningManagementSystem.Domain.Services.Abstract
+{
+    interface IRepository
+    {
+        StudentSubject FindByStudentAndSubjectId(int studentId, int subjectId);
+        void Save(StudentSubject studentSubject);
+    }
+}
---
+++ EntityFrameworkCore/Domain/Services/Model/ImportResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningManagementSystem.Domain.Services.Model
+{
+    public class ImportResult
+    {
+        public int SuccessfullyImported { get; set; }
+        public int Failed { get; set; }
+    }
+}
