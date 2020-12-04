c9694d41389a77e7e14ae248f3c9ceed8691f770
Archil Varshanidze <archil.varshanidze@wandiocom>
2020-12-04 20:18:02 +0400

    added seed data

---
+++ EntityFrameworkCore/Db/Migrations/DataSeeder.cs
@@ -0,0 +1,60 @@
+﻿using LearningManagementSystem.Domain.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningManagementSystem.Db.Migrations
+{
+    public class DataSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            var students = new List<Student>();
+
+            for (int i = 1; i < 51; i++)
+            {
+                students.Add(new Student
+                {
+                    StudentId = i,
+                    Name = $"Student {i}"
+                });
+            }
+
+            var subjects = new List<Subject>();
+
+            for (int i = 1; i < 6; i++)
+            {
+                subjects.Add(new Subject
+                {
+                    SubjectId = i,
+                    Name = $"Subject {i}"
+                });
+            }
+
+            var studentSubjects = new List<StudentSubject>();
+            Random random = new Random();
+
+            foreach (var student in students)
+            {
+                int firstSubjectIndex = random.Next(0, subjects.Count);
+                int secondSubjectIndex;
+
+                do
+                {
+                    secondSubjectIndex = random.Next(0, subjects.Count);
+                } while (firstSubjectIndex == secondSubjectIndex);
+
+
+                studentSubjects.AddRange(new List<StudentSubject> {
+                    new StudentSubject { StudentId = student.StudentId, SubjectId = subjects[firstSubjectIndex].SubjectId },
+                    new StudentSubject { StudentId = student.StudentId, SubjectId = subjects[secondSubjectIndex].SubjectId }
+                });
+            }
+
+            modelBuilder.Entity<Student>().HasData(students);
+            modelBuilder.Entity<Subject>().HasData(subjects);
+            modelBuilder.Entity<StudentSubject>().HasData(studentSubjects);
+        }
+    }
+}
--- EntityFrameworkCore/Db/StudentsDbContext.cs
+++ EntityFrameworkCore/Db/StudentsDbContext.cs
@@ -1,10 +1,15 @@
 ﻿using LearningManagementSystem.Domain.Model;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Linq;
+using Microsoft.Extensions.Caching.Memory;
+using EntityFrameworkCore.Db;
+using LearningManagementSystem.Db.Migrations;
 
 namespace EntityFrameworkCore.Db
 {
@@ -15,6 +20,7 @@ namespace EntityFrameworkCore.Db
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            DataSeeder.Seed(modelBuilder);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
