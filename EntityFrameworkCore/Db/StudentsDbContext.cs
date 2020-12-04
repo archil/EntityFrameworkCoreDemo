ec336163ada948ceaa931a5af95ac9e78aa2dd62
Archil Varshanidze <archil.varshanidze@wandiocom>
2020-12-04 19:04:34 +0400

    added base version

---
+++ EntityFrameworkCore/Db/StudentsDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace EntityFrameworkCore.Db
+{
+
+    public class StudentsDbContext : DbContext
+    {
+        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;database=BloggingDb;Integrated security=true;");
+
+            optionsBuilder.EnableSensitiveDataLogging(true);
+            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
+        }
+    }
+}
--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,5 +1,4 @@
-﻿using EntityFrameworkCore.EntityTypeConfigurations;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -13,65 +12,7 @@ namespace EntityFrameworkCore
     {
         static void Main(string[] args)
         {
-            using (var db = new BloggingDbContext())
-            {
-                var blogsCount = db.Blogs.Count();
 
-                Console.WriteLine($"There are {blogsCount} blogs in db");
-            }
         }
-    }
-
-    public class BloggingDbContext : DbContext
-    {
-        public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); });
-
-        public DbSet<Blog> Blogs { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Person>();
-            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("server=.;database=BloggingDb;Integrated security=true;");
-
-            optionsBuilder.EnableSensitiveDataLogging(true);
-            optionsBuilder.UseLoggerFactory(MyLoggerFactory);
-        }
-    }
-
-    public class Person
-    {
-        public int PersonId { get; set; }
-        public string Name { get; set; }
-    }
-
-    public class Car
-    {
-        public string LicencePlateNumber { get; set; }
-        public string Make { get; set; }
-    }
-
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-        public int Rating { get; set; }
-        public string Author { get; set; }
-        public DateTime BlogDate { get; set; }
-        public List<Post> Posts { get; set; }
-    }
-
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public int MyBlogForeignKeyValue { get; set; }
-        public Blog Blog { get; set; }
     }
 }
