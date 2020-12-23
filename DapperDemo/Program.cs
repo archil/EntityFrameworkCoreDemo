b4dd1c12831b3f8513c872c1cc0846f9704e44a1
Archil Varshanidze <archil.varshanidze@wandiocom>
2020-12-23 19:03:06 +0400

    added dapper demo

---
+++ DapperDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        public class Product
+        {
+            public int ProductId { get; set; }
+            public string Name { get; set; }
+            public int CategoryId { get; set; }
+            public decimal Price { get; set; }
+            public string Description { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection())
+            {
+
+            }
+        }
+    }
+}
